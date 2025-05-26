20a98dd880a00f0d6a58e828cac261222513f926
pandasmc <pandasmc@naver.com>
2025-05-26 16:33:49 +0900

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,27 @@
+import streamlit as st
+from pygwalker.api.streamlit import StreamlitRenderer
+import pandas as pd
  
+st.set_page_config(page_title="PyGWalker in Streamlit", layout="wide")
+st.title("PyGWalker in Streamlit Demo")
+ 
+@st.cache_resource
+def get_pyg_renderer() -> StreamlitRenderer:
+    df = pd.read_csv("data.csv")
+    return StreamlitRenderer(df, spec="./gw_config.json", spec_io_mode="rw")
+ 
+renderer = get_pyg_renderer()
+ 
+tab1, tab2, tab3 = st.tabs(["Explorer", "Data Profiling", "Charts"])
+ 
+with tab1:
+    renderer.explorer()
+ 
+with tab2:
+    renderer.explorer(default_tab="data")
+ 
+with tab3:
+    st.subheader("Registered per Weekday")
+    renderer.chart(0)
+    st.subheader("Registered per Day")
+    renderer.chart(1)
