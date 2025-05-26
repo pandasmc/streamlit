53ebb078e441e2c978af0843c44530f6ef9735cc
pandasmc <pandasmc@naver.com>
2025-05-26 16:43:50 +0900

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(
     layout="wide"
 )
 # Import your data
-df = pd.read_csv("https://kanaries-app.s3.ap-northeast-1.amazonaws.com/public-datasets/bike_sharing_dc.csv")
+df = pd.read_csv("https://github.com/pandasmc/streamlit/raw/refs/heads/main/data/%ED%99%98%EA%B2%BD%EB%B6%80%20%EA%B5%AD%EA%B0%80%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80%EC%A0%95%EB%B3%B4%EC%84%BC%ED%84%B0_%EA%B5%AD%EA%B0%80%20%EB%8C%80%EA%B8%B0%EC%98%A4%EC%97%BC%EB%AC%BC%EC%A7%88%20%EB%B0%B0%EC%B6%9C%EB%9F%89%20%ED%86%B5%EA%B3%84_20221231.csv")
  
 pyg_app = StreamlitRenderer(df)
  
